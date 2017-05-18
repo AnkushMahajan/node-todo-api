159bceb59a996465d3f4fc900070c83f32020305
Ankush Mahajan <vish_sukhoi@yahoo.co.in>
2017-05-18 13:28:59 +1000

    Added tests for routes

--- server/server.js
+++ server/server.js
@@ -21,8 +21,18 @@ app.post('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     })
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (err) => {
+        res.status(400).send(err);
+    })
 })
 
 app.listen('3000', () => {
     console.log('App is listening on port 3000!');
 });
+
+module.exports = {app};
